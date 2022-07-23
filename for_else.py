6445d61f3db891d058609043df4b73f96ab891e4
Sanskruti88 <109226352+Sanskruti88@users.noreply.github.com>
2022-07-23 12:13:08 +0530

    Create for_else.py

---
+++ for_else.py
@@ -0,0 +1,8 @@
+# for_else
+
+khana=["palak paneer","aloo paratha","moong daal halwa","paneer biryani"]
+for item in khana:
+    if item=="panner lasuni":
+        break
+else:
+    print("Your item is not found...")
